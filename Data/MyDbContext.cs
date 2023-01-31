ac9bfe75a44ce2f175b5736ac7c7db15a2c60646
unknown <afccarv21@gmail.com>
2023-01-30 21:59:10 -0300

    Agora vai, para ver o que está acontecendo

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoPessoa;
+
+namespace ProjetoPessoa
+{
+    public class appDbContext : DbContext
+    {
+        public appDbContext(DbContextOptions<appDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+
+    }
+}
+ 
